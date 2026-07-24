4b9dd84f99644bd58fdb6da6c63566d0d826e15f
hdz-kevin <kevinhdez592@gmail.com>
2026-07-24 17:42:33 -0600

    initialize fastapi app

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from fastsocial!")
-
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("app.app:app",  host="127.0.0.1", port=8000, reload=True)
