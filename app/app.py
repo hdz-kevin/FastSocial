38a4d22f3fb0b433cdf195aea45204813ec74866
hdz-kevin <kevinhdez592@gmail.com>
2026-07-27 17:02:22 -0600

    add PostResponse output type

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from app.schemas import PostCreate
+from app.schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -18,14 +18,14 @@ text_posts = {
 
 
 @app.get("/posts")
-def get_all_post(limit: int = None):
+def get_all_posts(limit: int = None):
     if limit:
         return list(text_posts.values())[:limit]
     return text_posts
 
 
 @app.get("/posts/{id}")
-def get_post(id: int) -> dict:
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(404, "Post not found")
 
@@ -33,7 +33,7 @@ def get_post(id: int) -> dict:
 
 
 @app.post("/posts")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostResponse:
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys()) + 1] = new_post
     return new_post
