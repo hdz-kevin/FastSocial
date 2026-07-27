e8dd7a3c889b502061d1025dc3bda1a1e099cb97
hdz-kevin <kevinhdez592@gmail.com>
2026-07-27 16:51:56 -0600

    add endpoint to create a post

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
@@ -29,3 +30,10 @@ def get_post(id: int) -> dict:
         raise HTTPException(404, "Post not found")
 
     return text_posts[id]
+
+
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
