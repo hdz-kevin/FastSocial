5d5b57c2c16a874967643a5516d7c761d68bb2f7
hdz-kevin <kevinhdez592@gmail.com>
2026-07-31 17:39:25 -0600

    feat: add feed endpoint

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.schemas import PostCreate, PostResponse
 from app.database import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -20,7 +21,7 @@ async def upload_post(
     caption: str = Form(...),
     session: AsyncSession = Depends(get_async_session),
 ):
-    post = Post(caption=caption, url="dummy_url", file_type="photo", file_name="dummy name")
+    post = Post(caption=caption, url="dummy_url", file_type="photo", file_name=file.filename)
 
     # Prepare the post object to be added to the database
     session.add(post)
@@ -31,3 +32,23 @@ async def upload_post(
 
     return post
 
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    post_data = []
+    for post in posts:
+        post_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+            }
+        )
+
+    return {"posts": post_data}
